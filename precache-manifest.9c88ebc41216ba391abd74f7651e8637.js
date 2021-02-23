6f77c8d33fd9a2697e1fb039e2aa17fd763a37fe
Siwaaa <siwaa@mail.ru>
2021-02-23 12:17:29 +0300

    Update .gitignore and add dist

---
+++ precache-manifest.9c88ebc41216ba391abd74f7651e8637.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "616b3ac6d5e552d2359e",
+    "url": "/training-vue-tasks-app/css/app.bd1681bb.css"
+  },
+  {
+    "revision": "616b3ac6d5e552d2359e",
+    "url": "/training-vue-tasks-app/js/app.79cacf13.js"
+  },
+  {
+    "revision": "7cc804107a2f573073c6",
+    "url": "/training-vue-tasks-app/js/chunk-2d0b64c8.3852db12.js"
+  },
+  {
+    "revision": "a098f7deb8e3b788ff9e",
+    "url": "/training-vue-tasks-app/js/chunk-58d99abc.2ffddf21.js"
+  },
+  {
+    "revision": "3f79e557a009af25201a",
+    "url": "/training-vue-tasks-app/css/chunk-6cdf8586.f4424221.css"
+  },
+  {
+    "revision": "3f79e557a009af25201a",
+    "url": "/training-vue-tasks-app/js/chunk-6cdf8586.b8d37b8d.js"
+  },
+  {
+    "revision": "5c454e6876cc86b6e5f8",
+    "url": "/training-vue-tasks-app/css/chunk-vendors.cac277ef.css"
+  },
+  {
+    "revision": "5c454e6876cc86b6e5f8",
+    "url": "/training-vue-tasks-app/js/chunk-vendors.c9fc0ea2.js"
+  },
+  {
+    "revision": "168684cfe1253c3839c520da27fe4e04",
+    "url": "/training-vue-tasks-app/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/training-vue-tasks-app/robots.txt"
+  }
+];
